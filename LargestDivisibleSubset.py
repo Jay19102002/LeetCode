0171edbb9ff4e17f73b0c51e51c432b2007583f3
JAY  DEWANGAN <143784616+Jay19102002@users.noreply.github.com>
2025-04-06 18:59:44 +0530

    368. Largest Divisible Subset

---
+++ LargestDivisibleSubset.py
@@ -0,0 +1,27 @@
+# Input: nums = [1,2,3]
+# Output: [1,2]
+# Explanation: [1,3] is also accepted.
+
+def findDiagonalOrder(nums):
+    nums.sort()
+    n = len(nums)
+    dp = [1] * n
+    max_size, max_index = 1, 0
+    for i in range(1, n):
+        for j in range(i):
+            if nums[i] % nums[j] == 0:
+                dp[i] = max(dp[i], dp[j] + 1)
+                if dp[i] > max_size:
+                    max_size = dp[i]
+                    max_index = i
+    result = []
+    num = nums[max_index]
+    for i in range(max_index, -1, -1):
+        if num % nums[i] == 0 and dp[i] == max_size:
+            result.append(nums[i])
+            num = nums[i]
+            max_size -= 1
+    return result
+
+nums = [1,2,3]
+print(findDiagonalOrder(nums)) 
